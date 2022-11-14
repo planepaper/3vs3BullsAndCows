b1af9aa63bf702cc5b80d00062d98204b9d47fb5
jdw1042710 <jdw1042710@gmail.com>
2022-11-14 18:21:39 +0900

    Add Player Rotation
    마우스 움직임에 따라 플레이어가 회전합니다.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,29 +4,50 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody rigidbody;
-    private Transform transform;
+    private Rigidbody rigid;
     private Animator animator;
     [SerializeField]
-    private float walkSpeed = 6.0f;
+    private float walkSpeed;
+    [SerializeField]
+    private float lookSensitivity;
 
     // Start is called before the first frame update
     void Awake()
     {
-        rigidbody = GetComponent<Rigidbody>();
-        transform = GetComponent<Transform>();
+        rigid = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Attack();
+    }
+    void FixedUpdate()
+    {
+        Move();
+        Rotation();
+    }
+
+    void Move() {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        bool attack = Input.GetButtonDown("Jump");
-        transform.Translate(new Vector3(h * walkSpeed * Time.deltaTime, 0, v * walkSpeed * Time.deltaTime));
+        Vector3 moveHorizontal = transform.right * h;
+        Vector3 moveVertical = transform.forward * v;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * walkSpeed;
+        rigid.MovePosition(transform.position + velocity * Time.deltaTime);
         animator.SetFloat("Horizontal", h, h * 0.1f, Time.deltaTime);
         animator.SetFloat("Vertical", v, v * 0.1f, Time.deltaTime);
+    }
+    void Rotation() {
+        float yRotation = Input.GetAxisRaw("Mouse X");
+        Vector3 charactorRotationY = new Vector3(0f, yRotation, 0f) * lookSensitivity;
+        rigid.MoveRotation(rigid.rotation * Quaternion.Euler(charactorRotationY));
+    }
+    void Attack() {
+        bool attack = Input.GetButtonDown("Fire1");
+
         if (attack)
         {
             animator.SetTrigger("Attack");
