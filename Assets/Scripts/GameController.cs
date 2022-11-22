5ce4f0a14178512ccc73bc1916f26d175415d8ce
jdw1042710 <jdw1042710@gmail.com>
2022-11-23 01:01:49 +0900

    feat: Add camera movement

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,10 +6,12 @@ using Photon.Realtime;
 
 public class GameController : MonoBehaviourPun
 {
+    public static GameController Instance;
     public GameObject player;
 
     private void Start()
     {
+        Instance = this;
         player = PhotonNetwork.Instantiate("Player", new Vector3(3.7f, 0f, 0f), Quaternion.identity);
     }
 
