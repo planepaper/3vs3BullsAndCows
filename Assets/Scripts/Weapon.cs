acf299e0074311233c126b8424f5225c51d76b52
Seungpyo Son <eddysonkr@gmail.com>
2022-11-24 16:33:26 +0900

    Revert "ADD Baseball Bat Collision Event with Player"
    This reverts commit da94f8ef854184ef722be296745bb412adc05669.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,24 +4,15 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    CapsuleCollider weaponCollider;
+    // Start is called before the first frame update
     void Start()
     {
-        weaponCollider = GetComponent<CapsuleCollider>();
-
-        weaponCollider.enabled = false;
+        
     }
 
-    public void Attack() {
-        StopCoroutine("Swing");
-        StartCoroutine("Swing");
-    }
-
-    // 0.15뒤 collider 활성화 0.30뒤 비활성화
-    private IEnumerator Swing() {
-        yield return new WaitForSeconds(0.10f);
-        weaponCollider.enabled = true;
-        yield return new WaitForSeconds(0.25f);
-        weaponCollider.enabled = false;
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
