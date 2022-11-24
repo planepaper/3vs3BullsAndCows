c9a80e92e00a2b6a414fe351e4af9383eddd5858
Seungpyo Son <eddysonkr@gmail.com>
2022-11-24 16:33:26 +0900

    Revert "Add Player Rotation"
    This reverts commit 4b0f21e564c941f2c35cce4276cb5a1045971e46.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,50 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody rigid;
+    private Rigidbody rigidbody;
+    private Transform transform;
     private Animator animator;
     [SerializeField]
-    private float walkSpeed;
-    [SerializeField]
-    private float lookSensitivity;
+    private float walkSpeed = 6.0f;
 
     // Start is called before the first frame update
     void Awake()
     {
-        rigid = GetComponent<Rigidbody>();
+        rigidbody = GetComponent<Rigidbody>();
+        transform = GetComponent<Transform>();
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Attack();
-    }
-    void FixedUpdate()
-    {
-        Move();
-        Rotation();
-    }
-
-    void Move() {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        Vector3 moveHorizontal = transform.right * h;
-        Vector3 moveVertical = transform.forward * v;
-
-        Vector3 velocity = (moveHorizontal + moveVertical).normalized * walkSpeed;
-        rigid.MovePosition(transform.position + velocity * Time.deltaTime);
+        bool attack = Input.GetButtonDown("Jump");
+        transform.Translate(new Vector3(h * walkSpeed * Time.deltaTime, 0, v * walkSpeed * Time.deltaTime));
         animator.SetFloat("Horizontal", h, h * 0.1f, Time.deltaTime);
         animator.SetFloat("Vertical", v, v * 0.1f, Time.deltaTime);
-    }
-    void Rotation() {
-        float yRotation = Input.GetAxisRaw("Mouse X");
-        Vector3 charactorRotationY = new Vector3(0f, yRotation, 0f) * lookSensitivity;
-        rigid.MoveRotation(rigid.rotation * Quaternion.Euler(charactorRotationY));
-    }
-    void Attack() {
-        bool attack = Input.GetButtonDown("Fire1");
-
         if (attack)
         {
             animator.SetTrigger("Attack");
