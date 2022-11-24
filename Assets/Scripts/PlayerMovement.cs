1ae4c56d455acb75db000fd78d9ecda502805ca1
Seungpyo Son <eddysonkr@gmail.com>
2022-11-24 16:33:26 +0900

    Revert "chore: Modity player retire"
    This reverts commit 9cc07d1ae6b4620a6beb8e898f90c014a68df792.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -62,8 +62,6 @@ public class PlayerMovement : MonoBehaviourPunCallbacks, IPunObservable
         if (health <= 0f) {
             //gameover;
             Debug.Log($"{photonView.Owner.NickName} is retired");
-            Destroy(this.gameObject);
-            return;
         }
     }
 
