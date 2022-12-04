c5a6594fab95a6fd599f165373e2a30d5c721244
byeori-moon <mblee2@naver.com>
2022-12-04 23:03:47 +0900

    야구공 40개 배치 완료

---
+++ Assets/Scripts/Baseball.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Realtime;
+using UnityEngine;
+
+public class Baseball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("들어옴");
+    }
+}
